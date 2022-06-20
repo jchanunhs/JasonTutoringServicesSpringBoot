0ba16c14bbe1f458c5c35d34629ac64ea154c512
jchanunhs <53135155+jchanunhs@users.noreply.github.com>
2022-06-19 21:32:44 -0400

    Remove unused imports

--- JasonTutoringServices/src/main/java/com/example/JasonTutoringServices/dao/AdminRepo.java
+++ JasonTutoringServices/src/main/java/com/example/JasonTutoringServices/dao/AdminRepo.java
@@ -1,11 +1,8 @@
 package com.example.JasonTutoringServices.dao;
 
 import com.example.JasonTutoringServices.model.Admin;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.Query;
-
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+
 
 public interface AdminRepo extends CrudRepository<Admin, Integer> {
 
